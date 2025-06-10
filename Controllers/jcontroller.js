9bbbb6ef0f1d226f41668d8df461316a4e38a1b3
jasleen-cyber <jasleenkmakkar7@gmail.com>
2025-06-10 23:54:38 +0530

    commit

--- Controllers/jcontroller.js
+++ Controllers/jcontroller.js
@@ -50,6 +50,21 @@ exports.getTour = (req, res) => {
 };
 };
 
+
+/get one tour
+exports.getTour = (req, res) => {
+  let id = req.params.id * 1;
+  l(id);
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours: tour,
+    },
+  });
+};
+};
+
 //get one tour
 exports.getTour = (req, res) => {
   let id = req.params.id * 1;
